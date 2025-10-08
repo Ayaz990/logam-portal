be2f8ef2d948b71b0ac21dfd3b658393ce3165c3
ayaz990 <ayazsmemon07@gmail.com>
2025-10-08 18:58:40 +0530

    Add Firebase authentication storage
    - Users are now stored in Firestore when they sign in
    - Using NextAuth Firebase adapter
    - Users collection will be automatically created
    - Sessions stored in database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { FirestoreAdapter } from '@next-auth/firebase-adapter'
+import { cert } from 'firebase-admin/app'
 
 const authOptions = {
   providers: [
@@ -13,23 +15,37 @@ const authOptions = {
       }
     })
   ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+  }),
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account, user }) {
       if (account) {
         token.accessToken = account.access_token
         token.refreshToken = account.refresh_token
       }
+      if (user) {
+        token.userId = user.id
+      }
       return token
     },
-    async session({ session, token }) {
+    async session({ session, token, user }) {
       session.accessToken = token.accessToken
       session.refreshToken = token.refreshToken
+      session.user.id = user?.id || token.userId
       return session
     }
   },
   pages: {
     signIn: '/auth/signin',
     error: '/auth/error'
+  },
+  session: {
+    strategy: 'database',
   }
 }
 
